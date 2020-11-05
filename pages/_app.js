edf4a5d83f42e2e60cac1d139dd79f063bee2ed3
Khristinasheer <khristinasar@gmail.com>
2020-11-05 17:53:39 -0400

    Initial setup, connect API and database to the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AmplifySignOut />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
